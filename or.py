d8dc8794072f4cdd6e5b65c7e9fc46650db4ea67
Vamsi <kvksteja1@gmail.com>
2022-09-19 11:05:10 +0530

    added logging and progress bar

--- or.py
+++ or.py
@@ -1,19 +1,24 @@
-import pandas as pd
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_plot, save_model
+import pandas as pd
+import logging
+import os
 
-def main(data,eta,epochs,filename,plotfilename):
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
 
+
+def main(data, modelName, plotName, eta, epochs):
     df = pd.DataFrame(data)
-    X,y = prepare_data(df)
-
+    logging.info(f"This is actual dataframe{df}")
+    X, y = prepare_data(df)
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X, y)
-
     _ = model.total_loss()
-
-    save_model(model,filename=filename)
-    save_plot(df,plotfilename,model)
+    save_model(model, filename=modelName)
+    save_plot(df, plotName, model)
 
 
 if __name__ == '__main__':
@@ -23,5 +28,11 @@ if __name__ == '__main__':
         "y": [0,1,1,1],
     }
     ETA = 0.3 # 0 and 1
-    EPOCHS = 10
-    main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotfilename="or.png")
+    EPOCHS = 100
+    try:
+        logging.info(">>>>> starting training >>>>>")
+        main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+        logging.info("<<<<< training done successfully<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
